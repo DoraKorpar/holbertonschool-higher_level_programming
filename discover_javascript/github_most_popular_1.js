34d7ec7ec0c50a00ef507c2744500df79be643be
Dora Korpar <28@p2-5.hbtn.io>
2016-02-15 19:57:34 -0800

    all exercises completed

--- discover_javascript/github_most_popular_1.js
+++ discover_javascript/github_most_popular_1.js
@@ -1,17 +1,16 @@
 var https = require('https')
 
-var token = process.env.TOKEN;
 var options = {
     hostname: 'api.github.com',
     path: '/search/repositories?q=language:javascript&sort=stars&order=desc',
     headers: {
 	'User-Agent': 'Holberton_School',
-	'Authorization': 'token '+token
+	'Authorization': 'token b803a2d7adb8b4ff31d4e5a6789cdec0c32f63b7'
     }
 };
 
 var req = https.request(options, function(result) {
-    console.log(result.statusCode);
+    console.log(result);
     result.on('data', function(d) {
 	process.stdout.write(d);
     });
---
+++ discover_javascript/write_them_to_disc.js
@@ -0,0 +1,38 @@
+var https = require('https')
+fs = require('fs')
+
+var options = {
+    hostname: 'api.github.com',
+    path: '/search/repositories?q=language:javascript&sort=stars&order=desc',
+    headers: {
+	'User-Agent': 'Holberton_School',
+	'Authorization': 'token b803a2d7adb8b4ff31d4e5a6789cdec0c32f63b7'
+    }
+};
+
+var req = https.request(options, function(res) {
+    streamToString(res, callback);
+});
+
+req.end();
+
+function streamToString(stream, cb) {
+    const chunks = [];
+    stream.on('data', (chunk) => {
+        chunks.push(chunk);
+    });
+    stream.on('end', () => {
+        cb(chunks.join(''));
+    });
+};
+
+var callback = function(jsonString) {
+    fs.writeFile('/tmp/28', jsonString)
+};
+
+
+req.on('error', function(e) {
+    console.error(e);
+});
+    
+    
