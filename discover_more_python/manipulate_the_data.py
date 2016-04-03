2ade99ffcd59cf4751846170fa792c5ab5c7ebec
dora <dora.korpar@gmail.com>
2016-04-02 21:29:04 -0700

    finished all mandatory

--- discover_more_python/manipulate_the_data.py
+++ discover_more_python/manipulate_the_data.py
@@ -1,4 +1,5 @@
 import urllib2
+import json
 
 request_headers = {
   'User-Agent': 'Holberton_School',
@@ -7,4 +8,11 @@ request_headers = {
 
 response = urllib2.urlopen('https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc')
 html = response.read()
-print html
+
+parsed_json = json.loads(html)
+
+for i in range(len(parsed_json["items"])):
+    print(parsed_json["items"][i]["full_name"])
+
+
+
