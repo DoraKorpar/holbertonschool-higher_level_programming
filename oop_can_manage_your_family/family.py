0dfbee74f02dea291647171b65b168851f7c3623
dora <dora.korpar@gmail.com>
2016-05-20 21:43:27 -0700

    finished third task

--- oop_can_manage_your_family/family.py
+++ oop_can_manage_your_family/family.py
@@ -1,9 +1,11 @@
+''' DECLARING PERSON CLASS '''
 class Person():
+
     ''' CLASS ATTRIBUTES '''
     EYES_COLORS = ["Blue", "Green", "Brown"]
     GENRES = ["Female", "Male"]
 
-    ''' CONSTRUCTOR '''
+    ''' CONSTRUCTOR and PRIVATE ATTRIBUTES '''
     def __init__(self, id, first_name, date_of_birth, genre, eyes_color):
         if type(id) is not int or id < 0:
             raise Exception("id is not an integer")
@@ -29,10 +31,11 @@ class Person():
             raise Exception("eyes_color is not valid")
         self.__eyes_color = eyes_color
 
-    ''' GETTERS '''
+    ''' PUBLIC ATTRIBUTE '''
     def last_name(self):
         self.last_name = last_name
 
+    ''' GETTERS '''
     def get_id(self):
         return self.__id
     
@@ -47,3 +50,62 @@ class Person():
 
     def get_first_name(self):
         return self.__first_name
+
+    ''' PUBLIC METHODS '''
+    def __str__(self):
+        return self.__first_name + " " + self.last_name
+
+    def is_male(self):
+        if self.__genre == "Male":
+            return True
+        else:
+            return False
+
+    def age(self):
+        if self.__date_of_birth[0] > 5 and self.__date_of_birth[1] > 20:
+            return (2015 - self.__date_of_birth[2])
+        else:
+            return (2016 - self.__date_of_birth[2])
+
+
+    ''' METHOD DEFAULTS FOR SUBCLASSES '''
+    def can_run(self):
+        return True
+
+    def need_help(self):
+        return True
+
+    def is_young(self):
+        return True
+    
+    def can_vote(self):
+        return True
+
+''' OTHER FAMILY CLASSES '''
+class Baby(Person):
+
+    def can_run(self):
+        return False
+    def can_vote(self):
+        return False
+
+class Teenager(Person):
+
+    def need_help(self):
+        return False
+    def can_vote(self):
+        return False
+
+class Adult(Person):
+
+    def need_help(self):
+        return False
+    def is_young(self):
+        return False
+
+class Senior(Person):
+
+    def can_run(self):
+        return False
+    def is_young(self):
+        return False
