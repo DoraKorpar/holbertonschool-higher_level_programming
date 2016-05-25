7a7b176c28270d69809ab73714ddd11282ce484d
dora <dora.korpar@gmail.com>
2016-05-24 17:54:19 -0700

    finished fourth task

--- manage_the_school_with_swift/person.swift
+++ manage_the_school_with_swift/person.swift
@@ -4,7 +4,7 @@ class Person {
 	var last_name: String
 	var age: Int
 
-	// initializing public attributes
+	// Constructor
 	init(first_name: String, last_name: String, age: Int) {
         	self.first_name = first_name
       	    	self.last_name = last_name
@@ -15,6 +15,11 @@ class Person {
       	func fullName() -> String {
       	   	return "\(self.first_name) \(self.last_name)"
       	}
+
+	// defining className method for subclasses
+	func className() -> String {
+		return "Person"
+	}
 }
 
 protocol Classify {
@@ -27,6 +32,11 @@ enum Subject {
 
 // subclass of Person with Classify protocol
 class Mentor: Person, Classify {
+	// method to determine class
+	override func className () -> String {
+		return "Mentor"
+	}
+
 	// defining isStudent method
 	func isStudent() -> Bool {
       	   	return false
@@ -41,7 +51,7 @@ class Mentor: Person, Classify {
 		super.init(first_name: first_name, last_name: last_name, age: age)
       	}
 
-	// defining function stringSubject
+	// defining function stringSubject, which returns string based on Subject
 	func stringSubject() -> String {
 		if self.subject == Subject.Math {
 			return "Math"
@@ -61,7 +71,42 @@ class Mentor: Person, Classify {
 
 // subclass of Person with Classify protocol
 class Student: Person, Classify {
+	// method to determine class
+	override func className () -> String {
+		return "Student"
+	}
+
       	func isStudent() -> Bool {
       	   	return true
       	}
+}
+
+// new class
+class School {
+	// public attributes
+	var name: String
+	var list_persons: [Person]
+
+	// Constructor
+	init(name: String) {
+		self.name = name
+		self.list_persons = []
+	}
+
+	// public methods
+	func addStudent(p: Person) -> Bool {
+		if p.className() == "Student" {
+			self.list_persons.append(p)
+			return true
+		}
+		return false
+	}
+
+	func addMentor(p: Person) -> Bool {
+		if p.className() == "Mentor" {
+			self.list_persons.append(p)
+			return true
+		}
+		return false
+	}
 }
