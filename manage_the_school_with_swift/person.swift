99bb4acb6202e4e9120fab50d07615291952417f
dora <dora.korpar@gmail.com>
2016-05-24 17:22:26 -0700

    finished 3rd mandatory task

--- manage_the_school_with_swift/person.swift
+++ manage_the_school_with_swift/person.swift
@@ -1,31 +1,67 @@
 class Person {
-      var first_name: String
-      var last_name: String
-      var age: Int
+	// declaring public attributes
+	var first_name: String
+	var last_name: String
+	var age: Int
 
-      init(first_name: String, last_name: String, age: Int) {
-            self.first_name = first_name
-      	    self.last_name = last_name
-      	    self.age = age
-      }
+	// initializing public attributes
+	init(first_name: String, last_name: String, age: Int) {
+        	self.first_name = first_name
+      	    	self.last_name = last_name
+      	    	self.age = age
+      	}
 
-      func fullName() -> String {
-      	   return "\(self.first_name) \(self.last_name)"
-      }
+	// defining fullName method
+      	func fullName() -> String {
+      	   	return "\(self.first_name) \(self.last_name)"
+      	}
 }
 
 protocol Classify {
-	 func isStudent() -> Bool
+	func isStudent() -> Bool
 }
 
+enum Subject {
+	case Math, English, French, History
+}
+
+// subclass of Person with Classify protocol
 class Mentor: Person, Classify {
-      func isStudent() -> Bool {
-      	   return false
-      }
+	// defining isStudent method
+	func isStudent() -> Bool {
+      	   	return false
+      	}
+
+	// defining constant subject
+      	var subject: Subject
+
+	// overloading constructor to include subject
+      	init(first_name: String, last_name: String, age: Int, subject: Subject = Subject.Math) {
+		self.subject = subject
+		super.init(first_name: first_name, last_name: last_name, age: age)
+      	}
+
+	// defining function stringSubject
+	func stringSubject() -> String {
+		if self.subject == Subject.Math {
+			return "Math"
+		}
+		if self.subject == Subject.English {
+			return "English"
+		}
+		if self.subject == Subject.French {
+			return "French"
+		}
+		if self.subject == Subject.History {
+			return "History"
+		}
+	return "String"
+	}
 }
 
+// subclass of Person with Classify protocol
 class Student: Person, Classify {
-      func isStudent() -> Bool {
-      	   return true
-      }
+      	func isStudent() -> Bool {
+      	   	return true
+      	}
 }
