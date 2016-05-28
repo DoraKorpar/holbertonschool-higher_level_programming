d8e8e2b03d7adbcaf96b5cd954b2ab5b4b59b0a7
Dora Korpar <28@p7-1.hbtn.io>
2016-05-28 11:16:20 -0700

    finished secondswift project

---
+++ welcome_to_silicon_valley/SiliconValleyCompanies/SiliconValleyCompanies/TechCompaniesHelper.swift
@@ -0,0 +1,19 @@
+//
+//  TechCompaniesHelper.swift
+//  SiliconValleyCompanies
+//
+//  Created by Dora Korpar on 5/27/16.
+//  Copyright © 2016 Dora Korpar. All rights reserved.
+//
+
+import Foundation
+
+class TechCompaniesHelper {
+
+    static var companies:[String] = ["Holberton", "Linkedin", "Docker", "Google", "Yahoo", "Apple"]
+    
+    static func getTechCompanies() -> [String] {
+        return companies
+    }
+    
+}
