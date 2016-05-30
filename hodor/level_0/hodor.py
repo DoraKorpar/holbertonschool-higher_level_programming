a9ef7516bc7b0a0f894e0c512348b6450a45fc84
dora <dora.korpar@gmail.com>
2016-05-30 14:27:14 -0700

    fixed print message

--- hodor/level_0/hodor.py
+++ hodor/level_0/hodor.py
@@ -4,7 +4,7 @@ url = "http://173.246.108.142/level0.php"
 data = ({'id': '28', 'holdthedoor': 'SUBMIT'})
 
 i = 0
-while i < 1023:
+while i < 1024:
     r = requests.post(url, data)
-    print ("Voted %d times" % i)
+    print ("Voted %d times" % (i + 1))
     i += 1
