df8e58510fc9ec04328be4981df241bfde35d780
dora <dora.korpar@gmail.com>
2016-06-05 12:57:38 -0700

    finished all but last mandatory

--- xml_and_json_are_the_base_of_web_2_0/car.py
+++ xml_and_json_are_the_base_of_web_2_0/car.py
@@ -1,27 +1,33 @@
 import json
+from xml.dom.minidom import Document
 
 class Car():
     # Constructor
     def __init__(self, *args, **kwargs):
         if len(args) > 0 and isinstance(args[0], dict):
             hash = args[0]
-            self.__name = hash.get('name')
-            self.__brand = hash.get('brand')
-            self.__nb_doors = hash.get('nb_doors')
+            name = hash.get('name')
+            brand = hash.get('brand')
+            nb_doors = hash.get('nb_doors')
         else:
-            self.__name = kwargs.get('name')
-            self.__brand = kwargs.get('brand')
-            self.__nb_doors = kwargs.get('nb_doors')
+            name = kwargs.get('name')
+            brand = kwargs.get('brand')
+            nb_doors = kwargs.get('nb_doors')
 
         # Attribute exceptions
-        if not self.__name or type(self.__name) != str:
+        if not name or type(name) != str:
             Exception("name is not a string")
-        if not self.__brand or type(self.__brand) != str:
+        if not brand or type(brand) != str:
             Exception("brand is not a string")
-        if type(self.__nb_doors) != int or self.__nb_doors < 0:
+        if type(nb_doors) != int or nb_doors < 0:
             Exception("nb_doors is not > 0")
 
-        # Gette
+        # Initialize
+        self.__name = name
+        self.__brand = brand
+        self.__nb_doors = nb_doors
+
+    # Getters
     def get_name(self):
         return self.__name
 
@@ -31,11 +37,36 @@ class Car():
     def get_nb_doors(self):
         return self.__nb_doors
 
+    # Setter
+    def set_nb_doors(self, number):
+        self.__nb_doors = number
+
+    # returns dict of attributes
     def to_hash(self):
         return { 'name': self.__name,
                  'brand': self.__brand,
                  'nb_doors': self.__nb_doors }
 
-    def __str__(self):
+    def to_json_string(self):
         json_string = json.dumps(self.to_hash())
         return json_string
+
+    def to_xml_node(self, doc):
+        car = doc.createElement('car')
+        car.setAttribute('nb_doors', str(self.__nb_doors))
+
+        name = doc.createElement('name')
+        name_content = doc.createCDATASection(self.__name)
+        name.appendChild(name_content)
+        car.appendChild(name)
+
+        brand = doc.createElement('brand')
+        brand_content = doc.createTextNode(self.__brand)
+        brand.appendChild(brand_content)
+        car.appendChild(brand)
+        
+        return car
+        
+    # returns string of dict of attributes
+    def __str__(self):
+        return "%s %s (%d)" % (self.__name, self.__brand, self.__nb_doors)
