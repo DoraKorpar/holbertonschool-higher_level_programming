69849c7acd59882332130fc005c536784f58f622
dora <dora.korpar@gmail.com>
2016-06-05 16:18:54 -0700

    capitalized all sql commands

--- basic_sql_playground/1-main.sql
+++ basic_sql_playground/1-main.sql
@@ -1,4 +1,5 @@
-SELECT first_name from Person;
-SELECT first_name, age from Person;
-SELECT distinct color from EyesColor;
-SELECT first_name, last_name, age from Person order by age;
+SELECT first_name FROM Person;
+SELECT first_name, age FROM Person;
+SELECT distinct color FROM EyesColor;
+SELECT first_name, last_name, age FROM Person
+ORDER BY age;
