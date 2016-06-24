7df010d0cd7fb7187904fe8e4eb2a8c185396f85
dora <dora.korpar@gmail.com>
2016-06-23 19:25:30 -0700

    finished second task and added comments

--- mysql_the_world_s_most_popular_open_source_database/0-main.sql
+++ mysql_the_world_s_most_popular_open_source_database/0-main.sql
@@ -1,19 +1,25 @@
+-- shows all tables
 SELECT 'List of all tables?' as '';
 SHOW TABLES;
 
+-- display how tables were made
 SELECT 'Display the table structure of TVShow, Genre, and TVShowGenre?' as '';
 SHOW CREATE TABLE TVShow;
 SHOW CREATE TABLE Genre;
 SHOW CREATE TABLE TVShowGenre;
 
+-- list tvshows ordered by name
 SELECT 'List of TVShows, only id and name ordered by name (A-Z)?' as '';
 SELECT id, name FROM TVShow ORDER BY name;
 
+-- list of genres in reverse alphabetical order
 SELECT 'List of Genres, only id and name ordered by name (Z-A)?' as '';
 SELECT id, name FROM Genre ORDER BY name DESC;
 
+-- list of networks
 SELECT 'List of Network, only id and name?' as '';
 SELECT id, name FROM Network;
 
+-- counts number of episodes 
 SELECT 'Number of episodes in the database?' as '';
 SELECT count(id) FROM Episode;
---
+++ mysql_the_world_s_most_popular_open_source_database/1-main.sql
@@ -0,0 +1,15 @@
+-- get number of seasons by tvshow_id
+\! echo "Number of seasons by tvshow_id?"
+SELECT tvshow_id, count(id) FROM Season GROUP BY tvshow_id ORDER BY tvshow_id;
+
+-- get number of occurrences of same episode number ordered by episode number
+\! echo "Number of occurrences of the same episode number ordered by episode number?"
+SELECT number, count(id) FROM Episode GROUP BY number ORDER BY number;
+
+-- get top 3 occurrences of genre in all TVshow, limit to top 3
+\! echo "Top 3 of the Genre\'s occurrences in all TVShows ordered by this number?"
+SELECT genre_id, count(genre_id) AS occurrences_genre FROM TVShowGenre GROUP BY genre_id ORDER BY count(genre_id) DESC LIMIT 3;
+
+-- get all tvshows containing string and print lowercase
+\! echo "Search all TVShow with this letter sequence \'th\' case insensitive and display with the name in lowercase?"
+SELECT LOWER(name) AS name FROM TVShow WHERE name LIKE '%th%';
